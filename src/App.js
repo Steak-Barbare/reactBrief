658e42b8eb1a81e64d0b17c05159a935f2544c6e
GrismoXO <piroskateur63@gmail.com>
2023-04-26 10:27:49 +0200

    romanian

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
+import Accueil from './Components/Accueil';
 
 function App() {
   return (
-
+    <Accueil />
   );
 }
 
---
+++ src/Components/Recette.jsx
@@ -0,0 +1,10 @@
+function Recette () {
+    return (
+        <div>
+            <h1>Accueil</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse totam dolore voluptate temporibus minus consequuntur itaque ut velit corporis excepturi fuga, debitis eos inventore quas accusamus quae molestias, odit qui.</p>
+        </div>
+    )
+}
+
+export default Recette;
