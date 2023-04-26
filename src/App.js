200a2cff608bc146bac1efd33d0ef7a290326172
Steak-Barbare <ddaredavid9@gmail.com>
2023-04-26 11:53:42 +0200

    maj routeur

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import './App.css';
-<<<<<<< HEAD
+import Header from './Components/Header';
 import Accueil from './Components/Accueil';
 
 function App() {
   return (
-    <Accueil />
-=======
-import Header from './Components/Header';
-
-
-function App() {
-  return (
-<Header/>
->>>>>>> 4f848f746979c4137249e84e3a9d6f33e78a7bd4
+    <div>
+      <Header/>
+      
+      <Accueil/>
+    </div>
   );
 }
+
+
 
 export default App;
