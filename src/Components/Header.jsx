4f848f746979c4137249e84e3a9d6f33e78a7bd4
Steak-Barbare <ddaredavid9@gmail.com>
2023-04-26 10:25:01 +0200

    modif2

---
+++ src/Components/Header.jsx
@@ -0,0 +1,21 @@
+function Header() {
+    return (
+        <div>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="">Acceuil</a>
+                    </li>
+                    <li>
+                        <a href="">Recettes</a>
+                    </li>
+                    <li>
+                        <a href="">Contact</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    );
+}
+
+export default Header;
